573a88d018c6d4edc79e720b11c8680ae90f83b7
Supi <supimon@gmail.com>
2020-01-02 18:14:25 +0530

    first commit

---
+++ src/main/java/com/supimon/ratingsservice/RatingsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.supimon.ratingsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsServiceApplication.class, args);
+	}
+
+}
